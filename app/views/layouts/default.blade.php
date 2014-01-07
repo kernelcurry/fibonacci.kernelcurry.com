6176a3a5cd9cb7c2dab1f6ce6b0fcb148adb6b54
michaelcurry <michael.ian.curry@gmail.com>
2014-01-07 13:11:46 -0800

    faecebook link moved

--- app/views/layouts/default.blade.php
+++ app/views/layouts/default.blade.php
@@ -30,6 +30,8 @@
 			<p class="text-center">
 				<a href="https://twitter.com/share" class="twitter-share-button" data-url="http://fibonacci.kernelcurry.com" data-text="Fibonacci Sequence Term Generator" data-via="kernelcurry">Tweet</a>
 				<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+				<div id="fb-root"></div>
+				<script>(function(d, s, id) { var js, fjs = d.getElementsByTagName(s)[0]; if (d.getElementById(id)) return; js = d.createElement(s); js.id = id; js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=360986720695289"; fjs.parentNode.insertBefore(js, fjs); }(document, 'script', 'facebook-jssdk')); </script>
 				<div class="fb-like" data-href="http://fibonacci.kernelcurry.com" data-layout="button_count" data-action="like" data-show-faces="false" data-share="false"></div>
 			</p>
 			<p class="text-center"><b>Authors Site:</b> <a href="http://kernelcurry.com">kernelcurry.com</a> <b>Source Code:</b> <a href="https://github.com/kernelcurry/fibonacci.kernelcurry.com">github</a></p>
